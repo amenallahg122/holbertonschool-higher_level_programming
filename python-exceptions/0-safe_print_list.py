66a2f308e7c20c7dd80765eeaf79ceca4d6a65e3
amenallah122 <garmaiamenallah@gmail.com>
2024-02-04 22:10:26 +0100

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        idx = 0
+        for x in range(x):
+            print('{}'.format(my_list[x]), end="")
+            idx += 1
+    except IndexError:
+        pass
+    print()
+    return idx
