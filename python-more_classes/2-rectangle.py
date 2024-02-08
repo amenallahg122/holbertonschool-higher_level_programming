03292ce581c60039d1c8d0310e779283ead8e850
amenallah122 <garmaiamenallah@gmail.com>
2024-02-08 20:57:22 +0100

    2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Rectangle."""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+        """ Private instance width """
+    @property
+    def width(self):
+        """with of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+        """ Private instance height """
+    @property
+    def height(self):
+        """height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+        """Public instance method calulate area"""
+
+    def area(self):
+        """area rectangle"""
+        return self.__width * self.__height
+
+        """Public instance method calculte perimetre"""
+
+    def perimeter(self):
+        """perimetre rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
