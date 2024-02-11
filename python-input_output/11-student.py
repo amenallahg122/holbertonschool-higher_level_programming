98bed390f2594d9a32cc490baafe1cb783fc90f4
amenallah122 <garmaiamenallah@gmail.com>
2024-02-11 21:47:31 +0100

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""python input output"""
+
+
+class Student:
+    """
+    a class Student that defines a student by: (based on 9-student.py)
+
+    * Public instance attributes:
+    first_name
+    last_name
+    age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            return {j: getattr(self, j) for j in attrs if hasattr(self, j)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for i, j in json.items():
+            setattr(self, i, j)
