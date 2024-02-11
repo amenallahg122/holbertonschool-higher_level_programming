b69057dfaf60911a0d6d5f827296082cd255afc5
amenallah122 <garmaiamenallah@gmail.com>
2024-02-11 20:02:59 +0100

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
     otherwise False
     """
 
-    if type(obj) == a_class:
-        return False
-    return isinstance(obj, a_class)
+    if issubclass(type(obj), a_class) and (type(obj) is not a_class):
+        return True
+    return False
